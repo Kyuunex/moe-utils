5cbd6de8c1dc346f33d55c3c06d526a18161b112
Kyuunex <kyuunex@protonmail.ch>
2025-06-01 00:14:35 +0400

    1.21.1 backport: migrate to mojang mappings, among other things

--- src/main/java/moe/kyuunex/moe_utils/utility/BlockUtils.java
+++ src/main/java/moe/kyuunex/moe_utils/utility/BlockUtils.java
@@ -8,23 +8,24 @@ import baritone.api.utils.RotationUtils;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Optional;
 
-import meteordevelopment.meteorclient.utils.player.FindItemResult;
 import meteordevelopment.meteorclient.utils.player.Rotations;
-import net.minecraft.block.Block;
-import net.minecraft.block.Blocks;
-import net.minecraft.block.FluidBlock;
-import net.minecraft.entity.Entity;
-import net.minecraft.network.packet.c2s.play.HandSwingC2SPacket;
-import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
-import net.minecraft.util.Hand;
-import net.minecraft.util.hit.BlockHitResult;
-import net.minecraft.util.hit.HitResult;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Box;
-import net.minecraft.util.math.Direction;
-import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.LiquidBlock;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.network.protocol.game.ServerboundSwingPacket;
+import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.level.material.Fluids;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.phys.AABB;
+import net.minecraft.core.Direction;
+import net.minecraft.world.phys.Vec3;
 import moe.kyuunex.moe_utils.modules.printer.PrinterUtils;
 
 public class BlockUtils {
@@ -34,44 +35,50 @@ public class BlockUtils {
         return Direction.values();
     }
 
+    public static boolean shouldAirPlace(BlockPos pos) {
+        for (Direction direction : getDirections()) {
+            if (!isReplaceable(pos.relative(direction))) return false;
+        }
+        return true;
+    }
+
     public static boolean isReplaceable(BlockPos pos) {
-        return mc.player != null && mc.player.getWorld().getBlockState(pos).isAir()
-            || mc.player.getWorld().getBlockState(pos).isReplaceable()
+        return mc.player != null && mc.player.level().getBlockState(pos).isAir()
+            || mc.player.level().getBlockState(pos).canBeReplaced()
             || isLiquid(pos);
     }
 
     public static boolean isLiquid(BlockPos pos) {
         return mc.player != null
-            && mc.player.getWorld().getBlockState(pos).getBlock() instanceof FluidBlock;
+            && mc.player.level().getBlockState(pos).getBlock() instanceof LiquidBlock;
     }
 
     public static boolean isNotAir(BlockPos pos) {
-        return mc.player == null || !mc.player.getWorld().getBlockState(pos).isAir();
+        return mc.player == null || !mc.player.level().getBlockState(pos).isAir();
     }
 
-    public static boolean canPlace(BlockPos pos) {
+    public static boolean canPlace(BlockPos pos, double dist) {
         assert mc.player != null;
 
         List<Entity> entities =
             mc.player
-                .getWorld()
-                .getOtherEntities(
-                    null,
-                    new Box(pos.toCenterPos().add(3, 3, 3), pos.toCenterPos().add(-3, -3, -3)),
+                .level()
+                .getEntities(null, new AABB(pos.getCenter().add(3, 3, 3), pos.getCenter().add(-3, -3, -3))).stream().filter(
                     entity -> {
                         if (EntityUtils.canPlaceIn(entity)) {
                             return false;
                         }
 
-                        return entity.collidesWithStateAtPos(pos, Blocks.BEDROCK.getDefaultState());
-                    });
+                        return entity.isColliding(pos, Blocks.BEDROCK.defaultBlockState());
+                    }).toList();
 
-        return isReplaceable(pos) && entities.isEmpty();
+        // Distance check needs improvement maybe ?
+        return isReplaceable(pos) && entities.isEmpty() && !shouldAirPlace(pos) && mc.player.getEyePosition().closerThan(getSafeHitResult(pos).getLocation(), dist);
     }
 
     public static Direction getPlaceDirection(BlockPos pos) {
         for (Direction direction : getDirections()) {
-            if (isNotAir(pos.offset(direction))) return direction;
+            if (isNotAir(pos.relative(direction))) return direction;
         }
         return Direction.UP;
     }
@@ -97,17 +104,17 @@ public class BlockUtils {
             }
 
             for (Direction direction : Direction.values()) {
-                Vec3d vec3d =
-                    new Vec3d(
-                        (double) pos.getX() + ((double) direction.getOpposite().getOffsetX() * 0.5),
-                        (double) pos.getY() + ((double) direction.getOpposite().getOffsetY() * 0.5),
-                        (double) pos.getZ() + ((double) direction.getOpposite().getOffsetZ() * 0.5));
+                Vec3 vec3d =
+                    new Vec3(
+                        (double) pos.getX() + ((double) direction.getOpposite().getStepX() * 0.5),
+                        (double) pos.getY() + ((double) direction.getOpposite().getStepY() * 0.5),
+                        (double) pos.getZ() + ((double) direction.getOpposite().getStepZ() * 0.5));
                 double yaw = Rotations.getYaw(vec3d), pitch = Rotations.getPitch(vec3d);
 
                 rotation =
                     RotationUtils.reachable(
                         BaritoneAPI.getProvider().getBaritoneForPlayer(mc.player).getPlayerContext(),
-                        pos.offset(direction),
+                        pos.relative(direction),
                         4.5);
 
                 if (rotation.isPresent()) {
@@ -120,17 +127,17 @@ public class BlockUtils {
                     return Map.entry((float) yaw, (float) pitch);
                 } else {
                     vec3d =
-                        new Vec3d(
-                            (double) pos.getX() + direction.getOpposite().getOffsetX(),
-                            (double) pos.getY() + direction.getOpposite().getOffsetY(),
-                            (double) pos.getZ() + direction.getOpposite().getOffsetZ());
+                        new Vec3(
+                            (double) pos.getX() + direction.getOpposite().getStepX(),
+                            (double) pos.getY() + direction.getOpposite().getStepY(),
+                            (double) pos.getZ() + direction.getOpposite().getStepZ());
                     yaw = Rotations.getYaw(vec3d);
                     pitch = Rotations.getPitch(vec3d);
 
                     rotation =
                         RotationUtils.reachable(
                             BaritoneAPI.getProvider().getBaritoneForPlayer(mc.player).getPlayerContext(),
-                            pos.offset(direction),
+                            pos.relative(direction),
                             4.5);
 
                     if (rotation.isPresent()) {
@@ -160,19 +167,19 @@ public class BlockUtils {
         }
 
         PrinterUtils.fakePlayer.setPos(mc.player.getX(), mc.player.getY(), mc.player.getZ());
-        PrinterUtils.fakePlayer.getAttributes().setFrom(mc.player.getAttributes());
+        PrinterUtils.fakePlayer.getAttributes().assignAllValues(mc.player.getAttributes());
         PrinterUtils.fakePlayer.setPose(mc.player.getPose());
-        PrinterUtils.fakePlayer.setPitch(pitch);
-        PrinterUtils.fakePlayer.prevPitch = pitch;
-        PrinterUtils.fakePlayer.setYaw(yaw);
-        PrinterUtils.fakePlayer.prevYaw = yaw;
-        PrinterUtils.fakePlayer.prevBodyYaw = yaw;
-        PrinterUtils.fakePlayer.prevHeadYaw = yaw;
-        PrinterUtils.fakePlayer.setHeadYaw(yaw);
-        PrinterUtils.fakePlayer.setBodyYaw(yaw);
-        PrinterUtils.fakePlayer.calculateDimensions();
+        PrinterUtils.fakePlayer.setXRot(pitch);
+        //PrinterUtils.fakePlayer.prevPitch = pitch;
+        PrinterUtils.fakePlayer.setYRot(yaw);
+        //PrinterUtils.fakePlayer.prevYaw = yaw;
+        //PrinterUtils.fakePlayer.prevBodyYaw = yaw;
+        //PrinterUtils.fakePlayer.prevHeadYaw = yaw;
+        PrinterUtils.fakePlayer.setYHeadRot(yaw);
+        PrinterUtils.fakePlayer.setYBodyRot(yaw);
+        PrinterUtils.fakePlayer.refreshDimensions();
 
-        HitResult pHitResult = PrinterUtils.fakePlayer.raycast(4, 1.0f, false);
+        HitResult pHitResult = PrinterUtils.fakePlayer.pick(4, 1.0f, false);
 
         if (pHitResult.getType() != HitResult.Type.BLOCK) {
             return false;
@@ -180,14 +187,18 @@ public class BlockUtils {
 
         BlockHitResult hitResult = (BlockHitResult) pHitResult;
 
-        return hitResult.getBlockPos().offset(hitResult.getSide()).equals(pos);
+        return hitResult.getBlockPos().relative(hitResult.getDirection()).equals(pos);
     }
 
-    public static Vec3d clickOffset(BlockPos pos) {
-        return new Vec3d(
-            (double) pos.getX() + ((double) getPlaceDirection(pos).getOffsetX() * 0.5),
-            (double) pos.getY() + ((double) getPlaceDirection(pos).getOffsetY() * 0.5),
-            (double) pos.getZ() + ((double) getPlaceDirection(pos).getOffsetZ() * 0.5));
+    public static Vec3 clickOffset(BlockPos pos) {
+        return new Vec3(
+            (double) pos.getX() + ((double) getPlaceDirection(pos).getStepX() * 0.5),
+            (double) pos.getY() + ((double) getPlaceDirection(pos).getStepY() * 0.5),
+            (double) pos.getZ() + ((double) getPlaceDirection(pos).getStepZ() * 0.5));
+    }
+
+    public static Vec3 clickOffset(BlockPos pos, Direction direction) {
+        return Vec3.atCenterOf(pos).add(direction.getStepX() * 0.5, direction.getStepY() * 0.5, direction.getStepZ() * 0.5);
     }
 
     public static BlockHitResult getBlockHitResult(
@@ -202,93 +213,164 @@ public class BlockUtils {
             }
 
             PrinterUtils.fakePlayer.setPos(mc.player.getX(), mc.player.getY(), mc.player.getZ());
-            PrinterUtils.fakePlayer.getAttributes().setFrom(mc.player.getAttributes());
+            PrinterUtils.fakePlayer.getAttributes().assignAllValues(mc.player.getAttributes());
             PrinterUtils.fakePlayer.setPose(mc.player.getPose());
-            PrinterUtils.fakePlayer.setPitch(rot.getValue());
-            PrinterUtils.fakePlayer.prevPitch = rot.getValue();
-            PrinterUtils.fakePlayer.setYaw(rot.getKey());
-            PrinterUtils.fakePlayer.prevYaw = rot.getKey();
-            PrinterUtils.fakePlayer.prevBodyYaw = rot.getKey();
-            PrinterUtils.fakePlayer.prevHeadYaw = rot.getKey();
-            PrinterUtils.fakePlayer.setHeadYaw(rot.getKey());
-            PrinterUtils.fakePlayer.setBodyYaw(rot.getKey());
-            PrinterUtils.fakePlayer.calculateDimensions();
+            PrinterUtils.fakePlayer.setXRot(rot.getValue());
+            //PrinterUtils.fakePlayer.prevPitch = rot.getValue();
+            PrinterUtils.fakePlayer.setYRot(rot.getKey());
+            //PrinterUtils.fakePlayer.prevYaw = rot.getKey();
+            //PrinterUtils.fakePlayer.prevBodyYaw = rot.getKey();
+            //PrinterUtils.fakePlayer.prevHeadYaw = rot.getKey();
+            PrinterUtils.fakePlayer.setYHeadRot(rot.getKey());
+            PrinterUtils.fakePlayer.setYBodyRot(rot.getKey());
+            PrinterUtils.fakePlayer.refreshDimensions();
 
-            return (BlockHitResult) PrinterUtils.fakePlayer.raycast(4.5, 1.0f, false);
+            return (BlockHitResult) PrinterUtils.fakePlayer.pick(4.5, 1.0f, false);
         }
         return new BlockHitResult(
             clickOffset(pos),
             direction == null ? getPlaceDirection(pos).getOpposite() : direction,
-            pos.offset(direction == null ? getPlaceDirection(pos) : direction.getOpposite()),
+            pos.relative(direction == null ? getPlaceDirection(pos) : direction.getOpposite()),
             false);
     }
 
+    public static double getHeight(BlockPos pos) {
+        return mc.player.clientLevel.getBlockState(pos).getCollisionShape(mc.player.clientLevel, pos).max(Direction.Axis.Y);
+    }
+
+    public static BlockHitResult getSafeHitResult(BlockPos pos) {
+        BlockPos.MutableBlockPos mutable = pos.mutable();
+        Direction direction = Direction.UP;
+
+        Vec3 offset;
+        double yHeight = 0;
+
+        for (Direction dir : getDirections()) {
+            // Performance!
+            mutable.set(pos.getX() + dir.getStepX(), pos.getY() + dir.getStepY(), pos.getZ() + dir.getStepZ());
+            if (!isReplaceable(mutable)) {
+                yHeight = getHeight(mutable);
+
+                direction = dir;
+
+                if (dir == Direction.DOWN) {
+                    break;
+                }
+            }
+        }
+
+        offset = clickOffset(pos, direction);
+
+        if (yHeight <= 0.6) {
+            // Don't ask why, know it works.
+            offset = new Vec3(offset.x, Math.floor(offset.y) + 0.0154, offset.z);
+        }
+
+        return new BlockHitResult(
+            offset,
+            direction.getOpposite(),
+            mutable.set(pos.getX() + direction.getStepX(),
+                pos.getY() + direction.getStepY(),
+                pos.getZ() + direction.getStepZ()),
+            false
+        );
+    }
+
     public static boolean placeBlock(
-        Hand hand, FindItemResult itemResult, BlockPos pos, long tickTimestamp) {
+        InteractionHand hand, BlockPos pos, long tickTimestamp) {
         assert mc.player != null : "Player has not joined the game.";
-        assert mc.interactionManager != null : "Interaction Manager is not defined.";
-        assert mc.getNetworkHandler() != null : "Network Handler is not defined.";
-        assert mc.world != null : "The world is null.";
+        assert mc.gameMode != null : "Somethings wrong here and it really shouldn't be.";
+        assert mc.getConnection() != null : "Somethings wrong here and it really shouldn't be. x2";
+        assert mc.level != null : "The world hasn't loaded yet.";
+
+        if (PrinterUtils.PRINTER.iHateGrim.get()) {
+            if (isReplaceable(pos)) {
+                mc.getConnection().getConnection().send(
+                    new ServerboundSwingPacket(hand),
+                    null,
+                    true
+                );
+                mc.gameMode.useItemOn(mc.player, hand, getSafeHitResult(pos));
+                return true;
+            }
+
+            return false;
+        }
 
         Direction dir = getPlaceDirection(pos);
 
-        boolean isCarpet =
-            mc.player
-                .getInventory()
-                .getStack(itemResult.slot())
-                .getItem()
-                .getTranslationKey()
-                .endsWith("carpet");
+        boolean isPartialBlock = getHeight(pos) < 0.6;
 
-        if (isCarpet && !PrinterUtils.PRINTER.raytraceCarpet.get()) {
+        if (isPartialBlock && !PrinterUtils.PRINTER.raytracePartial.get()) {
             dir = Direction.UP;
             Map.Entry<Float, Float> rot = getRotation(false, pos);
 
-            BlockPos offsetPos = pos.down();
+            BlockPos offsetPos = pos.below();
 
-            if (BlockUtils.isReplaceable(offsetPos)) {
-                Block block = mc.world.getBlockState(offsetPos).getBlock();
+            // Should work?
+            if (mc.level.getFluidState(offsetPos).is(Fluids.WATER) && !isLiquid(offsetPos)) {
+                Block block = mc.level.getBlockState(offsetPos).getBlock();
                 if (block == Blocks.KELP
                     || block == Blocks.KELP_PLANT
                     || block == Blocks.TALL_SEAGRASS
                     || block == Blocks.SEAGRASS) {
-                    mc.interactionManager.attackBlock(offsetPos, dir);
-                } else if (mc.world.getBlockState(offsetPos).getBlock() == Blocks.BUBBLE_COLUMN) {
-                    mc.interactionManager.interactBlock(
+                    mc.gameMode.startDestroyBlock(offsetPos, dir);
+                } else if (mc.level.getBlockState(offsetPos).getBlock() == Blocks.BUBBLE_COLUMN) {
+                    mc.getConnection().getConnection().send(
+                        new ServerboundSwingPacket(hand),
+                        null,
+                        true
+                    );
+                    mc.gameMode.useItemOn(
                         mc.player, hand, getBlockHitResult(false, offsetPos, dir));
                 } else {
                     return false;
                 }
             }
 
-            if (!mc.player.handSwinging) mc.getNetworkHandler().sendPacket(new HandSwingC2SPacket(hand));
-
             if (tickTimestamp == -1 || tickTimestamp != LAST_TIMESTAMP) {
                 LAST_TIMESTAMP = tickTimestamp;
-                mc.getNetworkHandler().sendPacket(
-                    new PlayerMoveC2SPacket.LookAndOnGround(
-                        rot.getKey(), rot.getValue(), mc.player.isOnGround()));
+                mc.getConnection().getConnection().send(
+                    new ServerboundMovePlayerPacket.Rot(
+                        rot.getKey(),
+                        rot.getValue(),
+                        mc.player.onGround()
+                    ),
+                    null,
+                    true
+                );
             }
 
-            // mc.interactionManager.interactBlock(mc.player, hand, getBlockHitResult(false, pos,
-            // getPlaceDirection(pos)));
-
-            mc.interactionManager.interactBlock(mc.player, hand, getBlockHitResult(false, pos, dir));
+            // mc.gameMode.useItemOn(mc.player, hand, getBlockHitResult(false, pos, getPlaceDirection(pos)));
+            mc.getConnection().getConnection().send(
+                new ServerboundSwingPacket(hand),
+                null,
+                true
+            );
+            mc.gameMode.useItemOn(mc.player, hand, getBlockHitResult(false, pos, dir));
             return true;
         }
 
         Map.Entry<Float, Float> rot = getRotation(true, pos);
 
         if (canRaycast(pos, rot.getValue(), rot.getKey()) || !PrinterUtils.PRINTER.raytraceFull.get()) {
-            if (!mc.player.handSwinging) {
-                mc.getNetworkHandler().sendPacket(new HandSwingC2SPacket(hand));
-            }
-            mc.getNetworkHandler().sendPacket(
-                new PlayerMoveC2SPacket.LookAndOnGround(rot.getKey(), rot.getValue(), mc.player.isOnGround())
+            mc.getConnection().getConnection().send(
+                new ServerboundMovePlayerPacket.Rot(
+                    rot.getKey(),
+                    rot.getValue(),
+                    mc.player.onGround()
+                ),
+                null,
+                true
             );
-            // mc.getNetworkHandler().sendPacket(new PlayerInteractBlockC2SPacket(hand, getBlockHitResult(true, pos, dir), 0));
-            mc.interactionManager.interactBlock(
-                mc.player, hand, getBlockHitResult(PrinterUtils.PRINTER.raytraceFull.get(), pos, dir));
+            // mc.getConnection().getConnection().send(new PlayerInteractBlockC2SPacket(hand, getBlockHitResult(true, pos, dir), 0), null, true);
+            mc.getConnection().getConnection().send(
+                new ServerboundSwingPacket(hand),
+                null,
+                true
+            );
+            mc.gameMode.useItemOn(
+                mc.player, hand, Objects.requireNonNull(getBlockHitResult(PrinterUtils.PRINTER.raytraceFull.get(), pos, dir)));
             return true;
         }
 
